9e2841329f10c5bee3570d323f9a5a5dc6fdd319
thendo90 <thenderson@protonmail.com>
2022-04-07 19:33:24 +0100

    refactor styling of ErrorPage

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,11 +1,11 @@
+import styles from "./ErrorPage.module.css";
 import { Link } from "react-router-dom";
-import "./ErrorPage.css";
 
 export default function ErrorPage({ message }) {
   return (
-    <div className="error__page">
+    <div className={styles.ErrorPage}>
       <h1>{message ? message : "You appear to be lost"}</h1>
-      <Link to="/" className="error-link">
+      <Link to="/" className={styles.ErrorPage__link}>
         Go home
       </Link>
     </div>
